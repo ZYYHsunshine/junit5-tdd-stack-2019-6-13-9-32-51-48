921e2eaa30c98aeb7a26554bc482869b56941ac0
ZYYHsunshine <zzucgg@163.com>
2019-08-23 17:17:24 +0800

    day5

--- src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
+++ src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
@@ -2,10 +2,8 @@ package com.thoughtworks.tdd;
 
 import com.thoughtworks.tdd.Dependency;
 import com.thoughtworks.tdd.HelloWorld;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import sun.plugin2.message.GetAppletMessage;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -51,4 +49,87 @@ public class HelloWorldTest {
         //then
         assertThat(actual, is("Hello World"));
     }
+
+    @Test
+    public void should_return_Fizz_when_input_2() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(2);
+
+        Assertions.assertEquals("2", result);
+
+    }
+
+
+    @Test
+    public void should_return_Fizz_when_input_3() {
+    Game game = new Game();
+
+    String result = game.fizzBuzz(3);
+
+    Assertions.assertEquals("Fizz", result);
+
 }
+
+    @Test
+    public void should_return_Fizz_when_input_5() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(5);
+
+        Assertions.assertEquals("Buzz", result);
+
+    }
+    @Test
+    public void should_return_Fizz_when_input_7() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(7);
+
+        Assertions.assertEquals("Wizz", result);
+
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_21() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(21);
+
+        Assertions.assertEquals("Wizz", result);
+
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_15() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(15);
+
+        Assertions.assertEquals("Wizz", result);
+
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_35() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(35);
+
+        Assertions.assertEquals("Wizz", result);
+
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_105() {
+        Game game = new Game();
+
+        String result = game.fizzBuzz(105);
+
+        Assertions.assertEquals("FizzBuzzWizz", result);
+
+    }
+}
+
+
+
